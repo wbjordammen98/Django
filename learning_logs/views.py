56e7fa5a809a12d9a0ad40c815f262aad203b709
Bjordammen <Will_Bjordammen1@baylor.edu>
2020-11-18 17:22:28 -0600

    code dump 2

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
+from .forms import EntryForm
 
 # Create your views here.
 def index(request):
@@ -20,3 +22,41 @@ def topic(request,topic_id):
     context = {'topic':topic,'entries':entries}
 
     return render(request,'learning_logs/topic.html',context)
+
+# Get = read data from database
+# post = sends data to database
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save() # Saves directly to topic model.
+
+            return redirect('learning_logs:topics')
+    
+    context = {'form':form}
+
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+
+            new_entry.topic = topic
+            new_entry.save()
+            form.save()
+
+            return redirect('learning_logs:topic',topic_id=topic_id)
+    
+    context = {'form':form,'topic':topic}
+    return render(request,'learning_logs/new_entry.html',context)
