b14dbf2e61e70831830a62968f46ba0a90db0a4f
wbjordammen98 <wbjordammen@gmail.com>
2020-12-01 02:15:14 -0600

    Added some finishing touches.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm
 from .forms import EntryForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -20,7 +21,7 @@ def topics(request):
 # To get individual topics.
 @login_required
 def topic(request,topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
 
     if topic.owner != request.user:
         raise Http404
